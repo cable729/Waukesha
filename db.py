aa0f6df715087393a4b093fd0ce402e96035933b
Dalton Dick <daltondick2010@gmail.com>
2013-04-09 12:51:01 -0500

    Mocked out some methods

--- db.py
+++ db.py
@@ -1,1 +1,6 @@
 # For interacting with the database (SQLite)
+
+
+def CreateDatabase(name = "default.db"):
+    print "Not implemented yet"
+    return
--- fb.py
+++ fb.py
@@ -1,1 +1,12 @@
-#For interacting with the Facebook API
+# For interacting with the Facebook API
+
+
+def GetAllFriends():
+    print "Not Implemented yet"
+    return
+
+
+def DownloadFriendPictures():
+    print "Not Implemented yet"
+    return
+
