76f3f4de818795ed5c059eacec23bf4fd9799197
Caleb Jares <cable729@gmail.com>
2013-04-21 15:08:23 -0500

    Takes a cam feed and crops the face out and saves it as temp.jpg

---
+++ facedetect.py
@@ -0,0 +1,68 @@
+#/usr/bin/env python
+
+import Image
+import numpy as np
+import cv2
+import cv2.cv as cv
+from video import create_capture
+from common import clock, draw_str
+
+help_message = '''
+USAGE: facedetect.py [--cascade <cascade_fn>] [--nested-cascade <cascade_fn>] [<video_source>]
+'''
+
+def detect(img, cascade):
+    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30), flags = cv.CV_HAAR_SCALE_IMAGE)
+    if len(rects) == 0:
+        return []
+    rects[:,2:] += rects[:,:2]
+    return rects
+
+def draw_rects(img, rects, color):
+    for x1, y1, x2, y2 in rects:
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
+
+if __name__ == '__main__':
+    import sys, getopt
+    print help_message
+
+    args, video_src = getopt.getopt(sys.argv[1:], '', ['cascade=', 'nested-cascade='])
+    try: video_src = video_src[0]
+    except: video_src = 0
+    args = dict(args)
+    cascade_fn = args.get('--cascade', "haarcascades/haarcascade_frontalface_alt.xml")
+
+    cascade = cv2.CascadeClassifier(cascade_fn)
+
+    cam = create_capture(video_src)
+
+    while True:
+        ret, img = cam.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.equalizeHist(gray)
+
+        t = clock()
+        rects = detect(gray, cascade)
+
+        vis = img.copy()
+        draw_rects(vis, rects, (0, 255, 0))
+        for x1, y1, x2, y2 in rects:
+            roi = gray[y1:y2, x1:x2]
+            vis_roi = vis[y1:y2, x1:x2]
+        dt = clock() - t
+
+        draw_str(vis, (20, 20), 'time: %.1f ms' % (dt*1000))
+        cv2.imshow('facedetect', vis)
+
+        # Check if it detected face
+        if len(rects) > 0:
+            crop_img = img[rects[0][1]:rects[0][3],rects[0][0]:rects[0][2]]
+            # resize image
+            image = Image.fromarray(crop_img)
+            thumb = image.resize((100,100), Image.BILINEAR)
+            break
+
+        if 0xFF & cv2.waitKey(5) == 27:
+            break
+    cv2.destroyAllWindows()
+
--- faces.py
+++ faces.py
@@ -1,12 +1,12 @@
 #For interacting with opencv functions
-import cv2 as cv
+import cv2.cv as cv
 
 HAAR_CASCADE_PATH = "C:\\opencv\\data\\haarcascades\\haarcascade_frontalface_default.xml"
 
 
 def DetectFacesInImage(image):
     faces = []
-    detected = cv.cv.HaarDetectObjects(cv.cv.LoadImage(image), cv.cv.Load(HAAR_CASCADE_PATH), cv.cv.CreateMemStorage(), 1.2, 2, cv.cv.CV_HAAR_DO_CANNY_PRUNING, (100,100))
+    detected = cv.HaarDetectObjects(cv.LoadImage(image), cv.Load(HAAR_CASCADE_PATH), cv.CreateMemStorage(), 1.2, 2, cv.CV_HAAR_DO_CANNY_PRUNING, (100,100))
     if detected:
         for (x, y, w, h), n in detected:
             faces.append((x, y, w, h))
